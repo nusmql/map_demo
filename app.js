8b0549f07447d341526e3fbd6d8509cc774412be
U-SG\Jack.Meng <Jack.Meng@SG217.sg.bwoil.com>
2016-10-31 13:17:16 +0800

    First Commit setup leaflet

---
+++ app.js
@@ -0,0 +1,9 @@
+// initialize the map one the map div with given center and zoom
+var map = L.map('map').setView([1.27511, 103.79846], 12);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+  maxZoom: 18,
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  id: 'mapbox.streets'
+}).addTo(map);
